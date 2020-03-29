60c834bcc6376a931a04c41a1f572045638e4696
puneet aggarwal <aggarwal.puneet222@gmail.com>
2020-03-29 18:59:47 +0530

    ARRAY-E-2

---
+++ remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<vector>
+#include<map>
+
+using namespace std;
+
+int removeDuplicates(vector<int>& nums) {
+        if(nums.size() <= 0) {
+            return 0;
+        }
+        int c = nums[0];
+        for(int i = 1; i < nums.size(); i++) {
+            if(c == nums[i]) {
+                nums.erase(nums.begin()+i);
+                i--;
+            }
+            else {
+                c = nums[i];
+            }
+        }
+        return nums.size();
+    }
