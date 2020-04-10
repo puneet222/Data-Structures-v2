d17af51b186d9de1812eb771381b3d185646d70c
puneet aggarwal <aggarwal.puneet222@gmail.com>
2020-04-11 02:19:49 +0530

    ARRAY-E-12

---
+++ missing-number.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <limits.h>
+
+using namespace std;
+
+int missingNumber(vector<int> &nums)
+{
+    bool last = false;
+    for (int i = 0; i < nums.size(); i++)
+    {
+        int index = abs(nums[i]);
+        if (index == nums.size())
+        {
+            last = true;
+        }
+        else
+        {
+            nums[index] = -nums[index];
+        }
+    }
+    if (!last)
+    {
+        return nums.size();
+    }
+    else
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] > 0)
+            {
+                return i;
+            }
+        }
+    }
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (nums[i] == 0)
+        {
+            return i;
+        }
+    }
+    return 0;
+}
